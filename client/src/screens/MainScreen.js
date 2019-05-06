f6519447eec407ecc30057f4b2dd22a2975fd350
mrkwon <erased11@gmail.com>
2019-05-06 20:46:37 +0900

    [추가] react-native-maps 컴포넌트 추가 !

--- client/src/screens/MainScreen.js
+++ client/src/screens/MainScreen.js
@@ -5,6 +5,7 @@ import {
   Button,
   StyleSheet
 } from 'react-native'
+import MapView from 'react-native-maps'
 
 import SplashScreen from './SplashScreen'
 
@@ -33,19 +34,25 @@ class MainScreen extends Component {
   }
   render() {
     // 개발시 불편해서 일단 주석 처리 !
-    // FIXME:
-    // MainScreen 의 헤더바가 SplashScreen 까지 침범
+    // FIXME: MainScreen 의 헤더바가 SplashScreen 까지 침범
+
     // if (!this.state.isMain) {
     //   return <SplashScreen />
     // }
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>MainScreen</Text>
-        <Button
-          title="Sign In"
-          onPress={() => {
-            this.props.navigation.navigate('Signin')
-          }}/>
+        <View style={styles.mapContainer}>
+          <MapView
+            style={{
+              flex: 1
+            }}
+            initialRegion={{
+              latitude: 37.5571,
+              longitude: 126.9426,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421
+          }}></MapView>
+        </View>
       </View>
     )
   }
@@ -54,9 +61,14 @@ class MainScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  mapContainer: {
+    flex: 1,
+    flexDirection: 'row'
   },
   text: {
     fontSize: 55,
